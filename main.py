f39d382c4c7a2abe94c5d7c91395fa2c75f02be7
Stephanie Gomez <70434067+StephanieGomez1211@users.noreply.github.com>
2024-06-15 16:49:08 +0800

    Update main.py
    Change update_resources to make_coffee.

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import art
 # from menu import resources,MENU, secretwords, coins
 from menu import *
 
-
 print(art.logo)
-
 profit: float = 0.0
 not_off: bool = True
 current_resources: dict = resources
-
 
 def get_key(order_resources: list[str]) -> list[str]:
     return [key for key,value in order_resources.items()]
@@ -17,15 +14,20 @@ def get_key(order_resources: list[str]) -> list[str]:
 def get_cost(ur_order: str) -> float:
     return MENU[ur_order]["cost"]
 
+
 def get_ingredients(ur_order: str, ingredient: str) -> int:
     return MENU[ur_order]["ingredients"][ingredient]
+
 
 def check(ur_order: str, ingredients: str) -> bool:
     return MENU[ur_order]["ingredients"][ingredients] <= current_resources[ingredients]
 
-def update_resources(ur_order: str) -> None:
+
+def make_coffee(ur_order: str) -> None:
     for ingredient in get_key(MENU[ur_order]['ingredients']):
         current_resources[ingredient] -= get_ingredients(ur_order,ingredient)
+
+
 
 def is_in_menu(menu: str) -> bool:
     return menu in MENU
@@ -40,20 +42,25 @@ def insert_coins(coin: str) -> float:
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
     return user_input
+
+
 def process_coins(ur_order: str, payment: float) -> float:
     if is_payment_enough(ur_order,payment):
         return payment - get_cost(ur_order)
     else:
         return payment
 
+
 def is_payment_enough(ur_order: str,payment: float) -> bool:
     return payment >=get_cost(ur_order)
+
 
 def profits(ur_order: str, payment: float) -> None:
     global profit
     profit += payment
     profit -= process_coins(ur_order, payment)
     # print(f" Profit: {profit}")
+
 
 def order(ur_order: str) -> None:
     global not_off
@@ -75,7 +82,7 @@ def order(ur_order: str) -> None:
             payment += float((insert_coins(key)) * float(coins[key]))
         print(f"Here is ${process_coins(ur_order,payment):.2f} change.")
         if is_payment_enough(ur_order,payment):
-            update_resources(ur_order)
+            make_coffee(ur_order)
             print(f"Here is your {ur_order} ☕ enjoy!")
         else:
             print(f"Sorry that's not enough money. Money Refunded.")
